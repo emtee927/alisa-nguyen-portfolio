3693c413b4eabf5cc73083626c6e7f4ec329c8ea
Alisa Nguyen <alisanguyen2709@gmail.com>
2026-04-14 12:12:36 -0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
-  });
-});
+// $(document).ready(function() {
+//   $('.experiment-image-1').click(function() {
+//     $('.experiment-image-2').fadeToggle("slow");
+//     // Alternative animation for example
+//     // slideToggle("fast");
+//   });
+// });
 
-$(document).ready(function() {
-  $('.work-page-grid-item').click(function() {
-    $('.work-page-grid-item').not(this).removeClass('active');
-    $(this).toggleClass('active');
+document.querySelectorAll('.work-page-grid-item').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.work-page-grid-item')
+      .forEach(i => i.classList.remove('active'));
+    
+    item.classList.add('active');
   });
 });
